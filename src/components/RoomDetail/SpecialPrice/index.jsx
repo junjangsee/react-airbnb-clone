fe94ebb8eb323ab26fcd7144aea90bd28fd3c180
junjangsee <junjang.see@gmail.com>
2020-11-13 14:20:07 +0900

    [#7]feat: 특별가 알람 컴포넌트 구현

--- src/components/RoomDetail/SpecialPrice/index.jsx
+++ src/components/RoomDetail/SpecialPrice/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import style from './index.module.scss';
-import classNames from 'classnames';
+import Wrapper from '../Wrapper';
+import Text from '../../Text';
 import Container from '../../Container';
+import Image from '../../Image';
+import alarm from '../../../images/alarm.gif';
 
-const SpecialPrice = ({ className }) => {
-  const classProps = classNames(style['special-wrapper'], className);
-
+const SpecialPrice = () => {
   return (
     <>
-      <Container className={classProps}>
-        <Container className={style['special-container']}></Container>
-      </Container>
+      <Wrapper className={style['special-wrapper']}>
+        <Container className={style['special-container']}>
+          <Container className={style['description']}>
+            <Text tag='h1'>11월 8일 - 11월 20일 동안 10% 할인</Text>
+            <Text>특별가 제안을 이용하려면 7일 이내에 예약하세요.</Text>
+          </Container>
+        </Container>
+        <Container className={style['alarm-container']}>
+          <Image src={alarm} width={40} heigth={40} />
+        </Container>
+      </Wrapper>
     </>
   );
 };
--- src/components/RoomDetail/Wrapper/index.jsx
+++ src/components/RoomDetail/Wrapper/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import style from './index.module.scss';
 import Container from '../../Container';
+import classNames from 'classnames';
 
-const Wrapper = ({ children }) => {
+const Wrapper = ({ className, children }) => {
+  const classProps = classNames(style['contents'], className);
+
   return (
     <>
       <Container className={style['wrapper']}>
         <Container className={style['container']}>
-          <Container className={style['contents']}>{children}</Container>
+          <Container className={classProps}>{children}</Container>
         </Container>
       </Container>
     </>
